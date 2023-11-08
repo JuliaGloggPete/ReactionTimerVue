bce744de787636304b817e6a7c5d89ac3417c533
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-11-08 10:08:58 +0100

    reaction

---
+++ src/components/Results.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <p >Reaction time = {{this.score}}  ms</p>
+    <p class="rank">{{ rank }}</p>
+</div>
+
+</template>
+
+<script>
+export default{
+props : ['score'],
+data(){
+
+    return{
+        rank: null
+    }
+    
+},
+mounted(){
+    if(this.score < 250){
+        this.rank = "Ninja reflex"
+    }
+    else if(this.score <400){
+        this.rank = "Good reflex"
+
+
+    }
+    else {
+        this.rank = "slow"
+    }
+}
+
+
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color:aquamarine;
+    font-weight: bold;
+}
+
+</style>
